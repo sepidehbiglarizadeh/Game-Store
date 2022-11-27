cca126e935790384087210e9674374bfcf2bc3b5
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2022-11-27 19:47:14 +0330

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      black: "#161a1e",
+      gray: "#9a9a9a",
+      orange: "#e58e27",
+      customWhite: "#fbfbfb",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
