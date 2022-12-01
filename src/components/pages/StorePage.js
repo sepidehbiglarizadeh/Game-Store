943cf2caa396aed9acb516bdb3a15e0d0f5e13ae
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2022-12-01 11:12:20 +0330

    export Products component

--- src/components/pages/StorePage.js
+++ src/components/pages/StorePage.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import getAllGamesService from "../../services/getAllGamesService";
+import Products from "../Products/Products";
 
 const StorePage = () => {
   const [games, setGames] = useState([]);
@@ -19,26 +20,8 @@ const StorePage = () => {
   console.log(games);
 
   return (
-    <div className="flex flex-wrap gap-10 text-left">
-      {games.map((game) => (
-        <div
-          key={game.id}
-          className="w-full md:max-w-[250px] p-4 glassMorphism h-80 flex flex-col justify-between"
-        >
-          <div className=" h-[50%] rounded-lg overflow-hidden">
-            <img src={game.image} className="w-full h-full" alt={game.name} />
-          </div>
-          <h2 className="text-customWhite font-bold">{game.name}</h2>
-          <div className="flex items-center">
-            <button className="flex-1 bg-orange rounded-3xl text-black p-2 font-bold">
-              خرید
-            </button>
-            <span className="text-orange font-bold text-lg mr-3">
-              ${game.price}
-            </span>
-          </div>
-        </div>
-      ))}
+    <div>
+      <Products games={games}/>
     </div>
   );
 };
