a9ad9f9ac8ab01ccb02e2071cd45963bf0d59e51
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2022-12-03 15:53:24 +0330

    apply filters

--- src/components/pages/StorePage.js
+++ src/components/pages/StorePage.js
@@ -10,6 +10,7 @@ const StorePage = () => {
     category: [],
     platform: [],
   });
+  const [filteredGames, setFilteredGames] = useState([]);
 
   const changeHandler = (e) => {
     if (e.target.checked) {
@@ -32,6 +33,7 @@ const StorePage = () => {
       try {
         const { data } = await getAllGamesService();
         setGames(data);
+        setFilteredGames(data);
       } catch (error) {
         console.log(error);
       }
@@ -39,16 +41,37 @@ const StorePage = () => {
     getGames();
   }, []);
 
+  const applyFilterHandler = () => {
+    const filterGames = games.filter((game) => {
+      return (
+        game.geners.some((gener) =>
+          filterValues.category.includes(gener.toLowerCase())
+        ) ||
+        game.platforms.some((p) =>
+          filterValues.platform.includes(p.name.toLowerCase())
+        )
+      );
+    });
+    if (filterGames.length) {
+      setFilteredGames(filterGames);
+    } else {
+      setFilteredGames(games);
+    }
+  };
+
   return (
     <div className="flex justify-between px-20">
       <div className="w-[55%] glassMorphism px-6 py-7 max-h-[calc(100vh_-_140px)] overflow-y-auto sticky top-28 scrollbar">
         <Categories changeHandler={changeHandler} />
         <Platforms changeHandler={changeHandler} />
-        <button className="w-full bg-orange text-black rounded-2xl py-2 font-bold">
+        <button
+          className="w-full bg-orange text-black rounded-2xl py-2 font-bold"
+          onClick={applyFilterHandler}
+        >
           اعمال فیلتر ها
         </button>
       </div>
-      <Products games={games} />
+      <Products games={filteredGames} />
     </div>
   );
 };
