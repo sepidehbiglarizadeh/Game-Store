e1f4a278e7941b7341504cf7a79d7b1ec6ead991
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2022-12-08 19:17:06 +0330

    log-out user

--- src/components/DesktopNavigation/DesktopNavigation.js
+++ src/components/DesktopNavigation/DesktopNavigation.js
@@ -1,16 +1,30 @@
-import { FaShoppingBasket, FaHeart,FaUserAlt } from "react-icons/fa";
+import { useState } from "react";
+import {
+  FaShoppingBasket,
+  FaHeart,
+  FaUserAlt,
+  FaChevronDown,
+} from "react-icons/fa";
 import { GiCrossedSlashes } from "react-icons/gi";
-import { NavLink } from "react-router-dom";
-import { useAuth } from "../../Providers/AuthProvider";
+import { Link, NavLink } from "react-router-dom";
+import { useAuth, useAuthActions } from "../../Providers/AuthProvider";
 import { useCart } from "../../Providers/CartProvider";
 
 const DesktopNavigation = () => {
   const { cart } = useCart();
+  const [isShow, setIsShow] = useState(false);
   const auth = useAuth();
+  const setAuth=useAuthActions();
+
+  const logoutUser=()=>{
+    localStorage.removeItem("authState");
+    setAuth(false);
+    setIsShow(false)
+  }
 
   return (
     <header className="hidden mb-4 md:block sticky top-0 left-0 right-0 z-10 bg-black">
-      <nav className="flex justify-between items-center text-xl">
+      <nav className="flex justify-between items-center text-xl relative">
         <ul className="flex justify-between items-center">
           <li className="flex items-center px-4 py-2">
             <GiCrossedSlashes size={50} className="fill-orange" />
@@ -61,15 +75,36 @@ const DesktopNavigation = () => {
               <FaHeart />
             </li>
           </NavLink>
-          <NavLink
-            to={auth ? "/profile":"/login"}
-            className={(navData) =>
-              navData.isActive ? "text-customWhite" : ""
-            }
-          >
-            <li className="px-4 py-2">{auth ? <FaUserAlt/>: "ورد/عضویت"}</li>
-          </NavLink>
+
+          <li className="px-4 py-2">
+            {auth ? (
+              <button
+                className="flex"
+                onClick={() => setIsShow((prevState) => !prevState)}
+              >
+                <FaUserAlt />
+                <FaChevronDown className="text-xs" />
+              </button>
+            ) : (
+              <NavLink
+                to="/login"
+                className={(navData) =>
+                  navData.isActive ? "text-customWhite" : ""
+                }
+              >
+                ورود/عضویت
+              </NavLink>
+            )}
+          </li>
         </ul>
+        {auth && (
+          <div className={`absolute left-0 -bottom-7 glassMorphism flex flex-col px-4 py-1 text-sm ${isShow ? "": "hidden"}`}>
+            <Link to="/profile" onClick={()=>setIsShow(false)}>
+              <span>{auth.user.name}</span>
+            </Link>
+            <span className="cursor-pointer" onClick={logoutUser}>خروج</span>
+          </div>
+        )}
       </nav>
     </header>
   );
