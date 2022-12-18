6e5e178865c1fb02755913be36f35eefedfaf449
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2022-12-18 15:08:58 +0330

    change endpoint

--- src/services/httpServices.js
+++ src/services/httpServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL ="http://localhost:3001";
+axios.defaults.baseURL ="https://my-json-server.typicode.com/sepidehbiglarizadeh/Game-Store";
 
 const http = {
   get: axios.get,
